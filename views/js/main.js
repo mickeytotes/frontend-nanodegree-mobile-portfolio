d3ec10a43469bf1f6e5d6fbfe463ab8ee9c87ec1
Michael Satow <satow.m@gmail.com>
2016-11-07 12:09:23 -0500

    actually do it

--- views/js/main.js
+++ views/js/main.js
@@ -503,8 +503,8 @@ function updatePositions() {
 
   var items = document.getElementsByClassName('mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop) / 1250) + (i % 5);
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    var phase = Math.sin(document.body.scrollTop / 1250) + (i % 5);
+    items[i].style.left = items[i].basicLeft + 100 * phase;
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
