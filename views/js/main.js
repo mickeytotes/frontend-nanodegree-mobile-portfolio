623f94a3f06a587b11fb339a75e75628a5187e77
Michael Satow <satow.m@gmail.com>
2016-11-07 12:32:21 -0500

    change back to 8 cols

--- views/js/main.js
+++ views/js/main.js
@@ -522,7 +522,7 @@ window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-  var cols = 5;
+  var cols = 8;
   var s = 256;
   for (var i = 0; i < 30; i++) {
     var elem = document.createElement('img');
